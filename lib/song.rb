b0e9050b315d98ca766629d8c4db6c973f1b99ad
James Scalise <jamescscalise@gmail.com>
2019-08-30 16:28:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,11 +41,11 @@ class Song
     @@genres.uniq
   end
   
-  def genre_count
+  def self.genre_count
     @@genre_count
   end
   
-  def artist_count
+  def self.artist_count
     @@artist_count
   end
 end
