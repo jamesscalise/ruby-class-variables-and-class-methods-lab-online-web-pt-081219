c66d57a704e0f349c2ac8e2a2513bb009cdb87cf
James Scalise <jamescscalise@gmail.com>
2019-08-30 16:07:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,10 @@ class Song
   
   def initialize(name, artist, genre)
     @@count += 1
-    name = name
-    artist = artist
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@artists << artist
+    @@genre << genre
     
 end
